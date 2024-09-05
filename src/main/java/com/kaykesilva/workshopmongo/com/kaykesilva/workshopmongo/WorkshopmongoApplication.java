cef98782b689ffc5a2df6978a40afb0199350e37
Kayke-S <kayke.allogins@gmail.com>
2024-09-05 15:32:59 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/main/java/com/kaykesilva/workshopmongo/com/kaykesilva/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,12 @@
+package com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
