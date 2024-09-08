d9ff6e6bed78b46ee88bc7ba574923b8b653750c
Kayke-S <kayke.allogins@gmail.com>
2024-09-08 17:55:33 -0300

    Consulta simples com @Query

--- src/main/java/com/kaykesilva/workshopmongo/com/kaykesilva/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/kaykesilva/workshopmongo/com/kaykesilva/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo.domain.Post;
@@ -16,5 +17,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	// faz o resto
 	
 	//ignoreCase eh uma 'assinatura' que ignora pesquisas maiusculas ou minusculas na url
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
