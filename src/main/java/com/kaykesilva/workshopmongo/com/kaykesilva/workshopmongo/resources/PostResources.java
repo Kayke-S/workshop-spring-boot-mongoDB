df56d4090cebc49607acad6c673ba09270d4b490
Kayke-S <kayke.allogins@gmail.com>
2024-09-10 23:30:26 -0300

    Consulta com varios criterios

--- src/main/java/com/kaykesilva/workshopmongo/com/kaykesilva/workshopmongo/resources/PostResources.java
+++ src/main/java/com/kaykesilva/workshopmongo/com/kaykesilva/workshopmongo/resources/PostResources.java
@@ -1,5 +1,6 @@
 package com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,24 +15,35 @@ import com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo.domain.Post;
 import com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo.resources.util.URL;
 import com.kaykesilva.workshopmongo.com.kaykesilva.workshopmongo.services.PostService;
 
-
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResources {
 
 	@Autowired
 	private PostService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Post> findById(@PathVariable String id){
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping(value="/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
 		text = URL.decodeParam(text); // decodifica o texto
 		List<Post> list = service.finByTitleCotaining(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+		    @RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text); // decodifica o texto
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 	
